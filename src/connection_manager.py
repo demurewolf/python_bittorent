3c5ad7a3b62086326a90145222cd10243d0eeeb6
demurewolf <josiahwedgwood@ymail.com>
2020-06-30 13:19:08 -0400

    Framework for connection manager added

--- src/connection_manager.py
+++ src/connection_manager.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+
+class ConnectionManager():
+
+    """
+    Maintains connections w/ swarm peers for download
+    """
+
+    def __init__(self, peers, info_hash):
+
+        self.peer_addrs = peers # This will contain ip/port info for each peer
+        self.info_hash = info_hash
+
+        self.peers = [] # This contians each connected peer's socket
+
+        super().__init__()
+
+    def initiate_connections(self):
+        pass
+    
+    def download_from_peers(self):
+        pass
+
+    def upload_to_peers(self):
+        pass
+
+    def terminate_connections(self):
+        pass
