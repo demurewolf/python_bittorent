7fa6af3a21dfa604984e14188b98dbf49eed5a6d
demurewolf <josiahwedgwood@ymail.com>
2020-07-10 01:21:08 -0400

    bit message library can decode byte data into tuple containing msg length, type, and data as a dictionary. Need to test for correctness

--- src/bittorrent_messages.py
+++ src/bittorrent_messages.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
-from struct import pack
+import logging
+from struct import pack, unpack
 
-# All messages are prefixed with the message length
+from bitarray import bitarray
 
+# Message codes used in the protocol
+CHOKE = 0
+UNCHOKE = 1
+INTERESTED = 2
+UNINTERESTED = 3
+HAVE = 4
+BITFIELD = 5
+REQUEST = 6
+PIECE = 7
+CANCEL = 8
+PORT = 9
 
 
 def handshake(info_hash: bytes, peer_id):
@@ -15,33 +27,100 @@ def keepalive():
     return b'\x00'
 
 def choke():
-    return pack('!ib', 1, 0)
+    return pack('!ib', 1, CHOKE)
 
 def unchoke():
-    return pack('!ib', 1, 1)
+    return pack('!ib', 1, UNCHOKE)
 
 def interested():
-    return pack('!ib', 1, 2)
+    return pack('!ib', 1, INTERESTED)
 
 def uninterested():
-    return pack('!ib', 1, 3)
+    return pack('!ib', 1, UNINTERESTED)
 
 def have(piece_index):
-    return pack('!ibi', 5, 4, piece_index)
+    return pack('!ibi', 5, HAVE, piece_index)
 
 def bitfield(pieces: bytes):
     msg_len = 1 + len(pieces)
-    return pack('!ib', msg_len, 5) + pieces
+    return pack('!ib', msg_len, BITFIELD) + pieces
 
 def request(piece_index, begin, length):
-    return pack('!ibiii', 13, 6, piece_index, begin, length)
+    return pack('!ibiii', 13, REQUEST, piece_index, begin, length)
 
 def piece(piece_index, begin, data: bytes):
     msg_len = 9
-    return pack('!ibii', msg_len, 7, piece_index, begin) + data
+    return pack('!ibii', msg_len, PIECE, piece_index, begin) + data
 
 def cancel(piece_index, begin, length):
-    return pack('!ibiii', 13, 8, piece_index, begin, length) 
+    return pack('!ibiii', 13, CANCEL, piece_index, begin, length) 
 
 def port(port_num):
-    return pack('!ibi', 5, 9, port_num)
+    return pack('!ibi', 5, PORT, port_num)
+
+def decode_message(buff):
+    if len(buff) >= 4:
+        msg_len = unpack('!i', buff[0:4])
+
+        if msg_len > 0:
+            msg_type = unpack('b', buff[4])
+
+            if msg_type >= HAVE:
+                param_switcher = {
+                    HAVE: _sw_have,
+                    BITFIELD: _sw_bitfield,
+                    REQUEST: _sw_request,
+                    PIECE: _sw_piece,
+                    CANCEL: _sw_cancel,
+                    PORT: _sw_port
+                }
+
+                params = param_switcher.get(msg_type)(buff[5:])
+
+                return (msg_len, msg_type, params)
+
+            else:
+                return (msg_len, msg_type)
+
+        else:
+            return (msg_len, )
+
+
+def _sw_have(buff):
+    return {
+        'piece_index': unpack('i', buff)
+    }
+
+def _sw_bitfield(buff):
+    return {
+        'bitfield': bitarray(buff)
+    }
+
+def _sw_request(buff):
+    (p_i, b, l) = unpack('iii', buff)
+    return {
+        'piece_index': p_i,
+        'begin': b,
+        'length': l
+    }
+
+def _sw_piece(buff):
+    (p_i, b) = unpack('ii', buff[0:8])
+    return {
+        'piece_index': p_i,
+        'begin': b,
+        'data': buff[8:]
+    }
+
+def _sw_cancel(buff):
+    (p_i, b, l) = unpack('iii', buff)
+    return {
+        'piece_index': p_i,
+        'begin': b,
+        'length': l
+    }
+
+def _sw_port(buff):
+    return {
+        'port': unpack('i', buff)
+    }
