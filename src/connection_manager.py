fe22d83d87681a363271629dd3acac5320c4969f
demurewolf <josiahwedgwood@ymail.com>
2020-07-12 02:04:03 -0400

    added logging

--- src/connection_manager.py
+++ src/connection_manager.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import logging
+
 from bittorrent_proxy import BitTorrentProxy
 
 class ConnectionManager():
@@ -15,18 +17,24 @@ class ConnectionManager():
         self._info_hash = info_hash
         self._server = event_server
 
-        self.peers = {} # This contians each connected peer's socket -> proxy obj
+        self._peers = {} # This contians each connected peer's socket -> proxy obj
 
         super().__init__()
 
     def initiate_connections(self):
+        logging.info("Initializing connections w/ peers list")
+
         for p in self._peer_addrs:
             addr = (p['ip'], p['port'])
             proxy = BitTorrentProxy(None, addr, self._peer_id, self._info_hash, self._server)
+            self._peers[proxy.sock] = proxy
             self._server.register_for_read_events(proxy.sock)
 
     def terminate_connections(self):
-        pass
+        logging.info("Terminating connections from proxy dictionary")
+        
+        for p in self._peers.values():
+            p.close_connection()
 
     def add_connection(self, peer_addr):
         self._peer_addrs.append(peer_addr)
